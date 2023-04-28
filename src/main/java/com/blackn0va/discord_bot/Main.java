4857bcb03d77023703cbc0595a8565b592a66f24
Blackn0va <blacknovaa@gmail.com>
2023-04-28 18:12:28 +0200

    .

--- src/main/java/com/blackn0va/discord_bot/Main.java
+++ src/main/java/com/blackn0va/discord_bot/Main.java
@@ -148,7 +148,7 @@ public class Main {
 
         //initial News
         rssNews.getPatchNotes();
-
+        //Translate.translate("test");
 
     }
 
---
+++ src/main/java/com/blackn0va/discord_bot/Translate.java
@@ -0,0 +1,21 @@
+package com.blackn0va.discord_bot;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Translate {
+
+    public static void translate(String text) throws IOException {
+
+        
+
+
+
+
+     }
+    
+}
